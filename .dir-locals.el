8a6bc5b800ea371e2029959dd7e071caba661787
Evan Klitzke <evan@eklitzke.org>
2011-10-11 00:08:46 -0700

    fix (some) style mistakes

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+;; For Emacs users, this will ensure that Emacs uses two-space indents for all
+;; of the files in the bjoern project.
+((nil . ((indent-tabs-mode . nil))))
--- uniq.cpp
+++ uniq.cpp
@@ -1,14 +1,15 @@
 /* a simple, non-comprehensive uniq(1) command */
 
-#include <iostream>
-#include <fstream>
-#include <string>
+/* get EXIT_SUCCESS, EXIT_FAILURE */
+#include <stdlib.h>
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
-/* get EXIT_SUCCESS, EXIT_FAILURE */
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 /* Display a value; this is either just the unique key, or the unique key and
  * the count
@@ -16,7 +17,7 @@ namespace po = boost::program_options;
 static inline void
 display_val(std::string key, unsigned int count, bool show_count) {
   if (show_count) {
-	std::cout << count << " ";
+    std::cout << count << " ";
   }
   std::cout << key << std::endl;
 }
@@ -26,19 +27,19 @@ static int
 uniq_file(std::string fname, bool show_count) {
   std::ifstream infile(fname.c_str(), std::ios::in);
   if (!infile) {
-	std::cerr << "no such file \"" << fname << "\"" << std::endl;
-	return EXIT_FAILURE;
+    std::cerr << "no such file \"" << fname << "\"" << std::endl;
+    return EXIT_FAILURE;
   }
   unsigned int count = 0;
   std::string prev = std::string("");
   for (std::string line; std::getline(infile, line); ) {
-	if (prev != line && prev != "") {
-	  display_val(prev, count, show_count);
-	  count = 1;
-	} else {
-	  count++;
-	}
-	prev = line;
+    if (prev != line && prev != "") {
+      display_val(prev, count, show_count);
+      count = 1;
+    } else {
+      count++;
+    }
+    prev = line;
   }
   display_val(prev, count, show_count);
   return EXIT_SUCCESS;
@@ -47,14 +48,12 @@ uniq_file(std::string fname, bool show_count) {
 int main(int argc, char **argv) {
   po::options_description generic("Generic options");
   generic.add_options()
-	("help,h", "print help message")
-	("count,c", "count input")
-	;
+    ("help,h", "print help message")
+    ("count,c", "count input");
 
   po::options_description hidden("Hidden options");
   hidden.add_options()
-	("input-file", po::value< std::vector<std::string> >(), "input file")
-	;
+    ("input-file", po::value< std::vector<std::string> >(), "input file");
 
   po::positional_options_description p;
   p.add("input-file", -1);
@@ -64,22 +63,23 @@ int main(int argc, char **argv) {
 
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).
-			options(cmdline_options).positional(p).run(), vm);
+            options(cmdline_options).positional(p).run(), vm);
 
   if (vm.count("help")) {
-	std::cout << generic << std::endl;
-	return EXIT_FAILURE;
+    std::cout << generic << std::endl;
+    return EXIT_FAILURE;
   }
 
-  bool show_count = (bool) vm.count("count");
+  bool show_count = static_cast<bool> (vm.count("count"));
 
-  std::vector<std::string> fnames = vm["input-file"].as< std::vector<std::string> >();
+  std::vector<std::string> fnames = vm["input-file"].as<
+    std::vector<std::string> >();
   std::vector<std::string>::iterator it;
   for (it = fnames.begin(); it < fnames.end(); it++) {
-	int ret = uniq_file(*it, show_count);
-	if (ret != EXIT_SUCCESS) {
-	  return ret;
-	}
+    int ret = uniq_file(*it, show_count);
+    if (ret != EXIT_SUCCESS) {
+      return ret;
+    }
   }
   return EXIT_SUCCESS;
 }
